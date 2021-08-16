19052565e6aacd43445c6dc1354128f8325aa67c
maksiz32 <maks-manzulin@mail.ru>
2021-08-17 01:50:33 +0300

    less1 готово

--- FoodProduct.php
+++ FoodProduct.php
@@ -1,1 +1,18 @@
 <?php
+
+include_once ("Product.php");
+
+/** Класс для продуктов
+* @param expiration - срок годности - текст в формате 'Y-m-d'
+*/
+
+class FoodProduct extends Product
+{
+    private string $expiration;
+
+    public function __construct(string $name, string $description, float $price, string $expiration)
+    {
+        parent::__construct($name, $description, $price);
+        $this->expiration = $expiration;
+    }
+}
--- Product.php
+++ Product.php
@@ -4,3 +4,41 @@
 5_2 - b1 - вызов уже совершенно нового класса, поэтому в каждом классе своя переменная X
 */
 
+class Product
+{
+    private string $name;
+    private string $description;
+    private float $price;
+
+    public function __construct(string $name, string $description, float $price)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function toDollars(float $course): ?float
+    {
+        return ($course > 0) ? $this->getPrice()/$course : null;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getName() . " (" . $this->getDescription() . "), Цена: " . $this->getPrice();
+    }
+}
